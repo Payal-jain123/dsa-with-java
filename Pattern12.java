f4d2ffe10d7c3e72b9047f47b37cfbc4c1a39ad2
Payal jain <166532121+Payal-jain123@users.noreply.github.com>
2024-04-24 20:39:14 +0530

    this is the diamond star pattern
    this is the diamond star pattern which is made by upper trangle pattern and downward triangle pattern..
    very interesting to make these pattern programs and this is very easy to male logics behind these programs..
    thanku

---
+++ Pattern12.java
@@ -0,0 +1,31 @@
+class Pattern12
+{
+public static void main(String[] args)
+{
+int row = 5;
+for(int i=0;i<=row;i++)
+{
+for(int j=(row-i);j>=0;j--)
+{
+System.out.print(" ");
+}
+for(int k=0;k<=i;k++)
+{
+System.out.print("* ");
+}
+System.out.println();
+}
+for(int i=row-1;i>=0;i--)
+{
+for(int j=(row-i);j>=0;j--)
+{
+System.out.print(" ");
+}
+for(int k=0;k<=i;k++)
+{
+System.out.print("* ");
+}
+System.out.println();
+}
+}
+}
