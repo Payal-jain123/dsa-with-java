b51d7073d6ee6cc3f2f940b77158879d39aa19e4
Payal jain <166532121+Payal-jain123@users.noreply.github.com>
2024-04-26 20:56:33 +0530

    optimal solution to rotate one place

---
+++ LeftRotateArray1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class LeftRotateArray1
+{
+ static  void solve(int arr[],int n)
+{
+int temp=arr[0];
+for(int i=0;i<n-1;i++)
+{
+arr[i]=arr[i+1];
+}
+arr[n-1]=temp;
+for(int i=0;i<n;i++)
+System.out.print(arr[i] +" ");
+}
+
+public static void main(String[] args)
+{
+int n=5;
+int arr[]={1,2,3,4,5};
+solve(arr,n);
+}
+}
