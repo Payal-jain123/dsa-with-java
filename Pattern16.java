96b4849033ff64c70af35a6bccb54b3adc652de3
Payal jain <166532121+Payal-jain123@users.noreply.github.com>
2024-05-01 22:10:34 +0530

    Add files via upload

---
+++ Pattern16.java
@@ -0,0 +1,19 @@
+class Pattern16
+{
+public static void main(String[] payal)
+{
+int row=6;
+for(int i=0;i<=row;i++)
+{
+for(int j=(row-i);j>=0;j--)
+{
+System.out.print("");
+}
+for(int k=0;k<=i;k++)
+{
+System.out.print( i+" ");
+}
+System.out.println();
+}
+}
+}
