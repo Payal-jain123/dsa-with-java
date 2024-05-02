80b79e21d45b8d3994d9275eb48cad380fcca934
Payal jain <166532121+Payal-jain123@users.noreply.github.com>
2024-05-02 22:48:32 +0530

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,17 @@
+class Main1 {
+   static void pattern4(int N)
+{
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+}
+    public static void main(String[] args) {
+        int N = 5;
+        pattern4(N);
+    }
+}
