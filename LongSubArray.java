6e9c8e05888567d5cbfda9af6102ef97b40cb48c
Payal jain <166532121+Payal-jain123@users.noreply.github.com>
2024-06-26 08:56:03 +0530

    brute force approach 1

---
+++ LongSubArray.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class LongSubArray {
+    public static int getLongestSubarray(int []a, long k) {
+        int n = a.length; // size of the array.
+
+        int len = 0;
+        for (int i = 0; i < n; i++) { // starting index
+            for (int j = i; j < n; j++) { // ending index
+                // add all the elements of
+                // subarray = a[i...j]:
+                long s = 0;
+                for (int K = i; K <= j; K++) {
+                    s += a[K];
+                }
+
+                if (s == k)
+                    len = Math.max(len, j - i + 1);
+            }
+        }
+        return len;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1,2,3,5,4,7,9,3};
+
+        long k = 15;
+        int len = getLongestSubarray(a, k);
+        System.out.println("The length of the longest subarray is: " + len);
+    }
+}
+
