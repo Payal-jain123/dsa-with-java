0e1e7ee72cc0374df631fbbc15cc8619db4d59f7
Payal jain <166532121+Payal-jain123@users.noreply.github.com>
2024-07-01 13:36:50 +0530

    Add files via upload

---
+++ Sumoftwonumbers.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Sumoftwonumbers1
+{
+public static void main(String[]args)
+{
+Scanner sc = new Scanner(System.in);
+int a = sc.nextInt();
+int b = sc.nextInt();
+int sum = a+b;
+System.out.println("enter two numbers: "+sum);
+
+}
+}
+/*import java.util.Scanner;  
+public class Sumoftwonumbers
+ {  
+public static void main(String args[])  
+{  
+int x, y, sum;  
+Scanner sc = new Scanner(System.in);  
+System.out.print("Enter the first number: ");  
+x = sc.nextInt();  
+System.out.print("Enter the second number: ");  
+y = sc.nextInt();  
+sum = x+y;
+System.out.println("The sum of two numbers x and y is: " + sum);  
+}  
+}*/
