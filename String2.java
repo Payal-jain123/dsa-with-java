8b33c02d8456d7654ae839d71d43de590ea4ce0d
Payal jain <166532121+Payal-jain123@users.noreply.github.com>
2024-07-02 15:44:05 +0530

    Add files via upload

---
+++ String2.java
@@ -0,0 +1,12 @@
+class String3
+{
+public static void main(String[]args)
+{
+String s1=new String("payal");
+String s2=new String("payal");
+String s3= "payal";
+String s4= "payal";
+System.out.println(s1==s2);
+System.out.println(s1.equals(s3));
+}
+}
