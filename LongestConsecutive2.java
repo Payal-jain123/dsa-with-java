af82fdd828e1d7a47c738a1cc508bb6968a40162
Payal jain <166532121+Payal-jain123@users.noreply.github.com>
2024-07-08 11:20:05 +0530

    Add files via upload

---
+++ LongestConsecutive2.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class LongestConsecutive2 {
+    public static int longestConsecutive(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+
+        int longest = 0;
+
+        for (int num : numSet) {
+            if (!numSet.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (numSet.contains(currentNum + 1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+
+                longest = Math.max(longest, currentStreak);
+            }
+        }
+
+        return longest;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {100, 4, 200, 1, 3, 2};
+        System.out.println("Longest consecutive sequence: " + longestConsecutive(nums));
+    }
+}
