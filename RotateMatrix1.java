4b04e95f08bcfdaa2ff87669b0a91e4409af7fda
Payal jain <166532121+Payal-jain123@users.noreply.github.com>
2024-07-09 07:24:21 +0530

    optimal

---
+++ RotateMatrix1.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class RotateMatrix1 {
+    public static int[][] rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+
+        return matrix;
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        matrix = rotate(matrix);
+        System.out.println("Rotated Image is");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
