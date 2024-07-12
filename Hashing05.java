c90fb9a22c719d2ea6c384edec9347165e5829f7
Payal jain <166532121+Payal-jain123@users.noreply.github.com>
2024-07-12 08:12:49 +0530

    Add files via upload

---
+++ Hashing05.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Hashing06{
+public static void main(String[] args){
+System.out.println("Enter a string");
+Scanner sc=new Scanner(System.in);
+String s;
+s=sc.next();
+
+int[] hash=new int[256];
+for(int i=0;i<s.length();i++){
+hash[s.charAt(i)]+=1;
+}
+int q;
+System.out.println("Enter the Size we found:");
+q=sc.nextInt();
+while(q-->0){
+char c;
+System.out.println("enter characters:");
+c=sc.next().charAt(0);
+System.out.println(hash[c]);
+}
+}
+}
+
+
