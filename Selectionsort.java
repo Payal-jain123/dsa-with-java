8d0a077861dab3dca0363b8c91ebd7ef954f20b8
Payal jain <166532121+Payal-jain123@users.noreply.github.com>
2024-07-13 12:51:01 +0530

    Add files via upload

--- Selectionsort.java
+++ Selectionsort.java
@@ -1,41 +1,31 @@
-import java.util.Scanner;  
-  
-public class Selectionsort  
-{  
-   public static void main(String args[])  
-   {  
-       int size, i, j, temp;  
-       int arr[] = new int[10];  
-
-       Scanner scan = new Scanner(System.in);  
-         
-       System.out.print("Enter Array Size : ");  
-       size = scan.nextInt();  
-         
-       System.out.print("Enter Array Elements : ");  
-       for(i=0; i<size; i++)  
-       {  
-           arr[i] = scan.nextInt();  
-       }  
-         
-       System.out.print("Sorting Array using Selection Sort Technique..\n");  
-       for(i=0; i<size; i++)  
-       {  
-           for(j=i ; j < size ; j++)  
-           {  
-               if(arr[i] > arr[j])  
-               {  
-                   temp = arr[i];  
-                   arr[i] = arr[j];  
-                   arr[j] = temp;  
-               }  
-           }  
-       }  
-         
-       System.out.print("Now the Array after Sorting is :\n");  
-       for(i=0; i<size; i++)  
-       {  
-           System.out.print(arr[i]+ "  ");  
-       }  
-   }  
-}  
+import java.util.*;
+class SelectionSort{
+public static void selection(int arr[],int n){
+for(int i=0;i<=n-2;i++){
+int mini=i;
+for(int j=i;j<=n-1;j++){
+if(arr[j]<arr[mini]){
+mini=j;
+}
+}
+int temp=arr[mini];
+arr[mini]=arr[i];
+arr[i]=temp;
+}
+System.out.print("After Selection Sort: ");
+for(int i=0;i<n;i++){
+System.out.print(arr[i] + " ");
+}
+System.out.println();
+}
+public static void main(String[] args){
+int[] arr={9,24,54,78,12,94,23,18};
+int n=arr.length;
+System.out.print("Before selection sort : ");
+for(int i=0;i<n;i++){
+System.out.print(arr[i] + " ");
+}
+System.out.println();
+selection(arr,n);
+}
+}
