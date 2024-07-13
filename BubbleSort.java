8a661d8a21c2ba4b10d5f28a85bb476fa34b4b2c
Payal jain <166532121+Payal-jain123@users.noreply.github.com>
2024-07-13 12:51:58 +0530

    brute force with worst and average approach 0(n^2)

---
+++ BubbleSort.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class BubbleSort{
+public static void bubble(int[] arr,int n){
+for(int i=n-1;i>=0;i--){
+for(int j=0;j<=i-1;j++){
+if(arr[j]>arr[j+1]){
+int temp=arr[j];
+arr[j]=arr[j+1];
+arr[j+1]=temp;
+}
+}
+}
+System.out.println("After Bubble Sort: ");
+for(int i=0;i<n;i++){
+System.out.print(arr[i] +" ");
+}
+System.out.println();
+}
+
+public static void main(String[] args){
+int[] arr={4,7,23,56,87,14};
+int n=arr.length;
+System.out.println("Before Bubble Sort: ");
+for(int i=0;i<n;i++)
+{ 
+System.out.print(arr[i] +" ");
+}
+System.out.println();
+bubble(arr,n);
+}
+}
