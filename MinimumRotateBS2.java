9b0ea1936c8358958b1e90a19342cab02411d2fa
Payal jain <166532121+Payal-jain123@users.noreply.github.com>
2024-07-17 08:38:41 +0530

    Add files via upload

---
+++ MinimumRotateBS2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class MinimumRotateBS2 {
+    public static int findMin(int []arr) {
+        int low = 0, high = arr.length - 1;
+        int ans = Integer.MAX_VALUE;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            //if left part is sorted:
+            if (arr[low] <= arr[mid]) {
+                // keep the minimum:
+                ans = Math.min(ans, arr[low]);
+
+                // Eliminate left half:
+                low = mid + 1;
+
+            } else { //if right part is sorted:
+
+                // keep the minimum:
+                ans = Math.min(ans, arr[mid]);
+
+                // Eliminate right half:
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2, 3};
+        int ans = findMin(arr);
+        System.out.println("The minimum element is: " + ans );
+    }
+}
+
