08d2230f48624d489433f2f29d15d2fed4a54af8
Payal Jain <166532121+Payal-jain123@users.noreply.github.com>
2024-07-21 13:40:37 +0530

    brute

---
+++ Sum3.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Sum3 {
+    public static List<List<Integer>> triplet(int n, int[] arr) {
+        Set<List<Integer>> st = new HashSet<>();
+
+        // check all possible triplets:
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (arr[i] + arr[j] + arr[k] == 0) {
+                        List<Integer> temp = Arrays.asList(arr[i], arr[j], arr[k]);
+                        temp.sort(null);
+                        st.add(temp);
+                    }
+                }
+            }
+        }
+
+        // store the set elements in the answer:
+        List<List<Integer>> ans = new ArrayList<>(st);
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { -1, 0, 1, 2, -1, -4};
+        int n = arr.length;
+        List<List<Integer>> ans = triplet(n, arr);
+        for (int i=0;i<ans.size();i++) {
+            System.out.print(ans.get(i) + "");
+            }
+System.out.println();
+}
+}
