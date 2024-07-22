cd4ac88614c9244d174f6e946ce23ad1fcbaba95
Payal Jain <166532121+Payal-jain123@users.noreply.github.com>
2024-07-22 09:15:58 +0530

    optimal1 without using gap

---
+++ Merge2sorted.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Merge2sorted {
+
+    public static void merge(long[] arr1, long[] arr2, int n, int m) {
+
+        // Declare 2 pointers:
+        int left = n - 1;
+        int right = 0;
+
+        // Swap the elements until arr1[left] is
+        // smaller than arr2[right]:
+        while (left >= 0 && right < m) {
+            if (arr1[left] > arr2[right]) {
+                long temp = arr1[left];
+                arr1[left] = arr2[right];
+                arr2[right] = temp;
+                left--;
+                right++;
+            } else {
+                break;
+            }
+        }
+
+        // Sort arr1[] and arr2[] individually:
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+    }
+
+    public static void main(String[] args) {
+        long[] arr1 = {1, 4, 8, 10};
+        long[] arr2 = {2, 3, 9};
+        int n = 4, m = 3;
+        merge(arr1, arr2, n, m);
+        System.out.println("The merged arrays are:");
+        System.out.print("arr1[] = ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr1[i] + " ");
+        }
+        System.out.print("\narr2[] = ");
+        for (int i = 0; i < m; i++) {
+            System.out.print(arr2[i] + " ");
+        }
+        System.out.println();
+    }
+
+}
