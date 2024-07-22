6a9f3447edb5ecfd7d8cc1d5314105d4a45a7415
Payal Jain <166532121+Payal-jain123@users.noreply.github.com>
2024-07-22 08:36:26 +0530

    Add files via upload

---
+++ XORk.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class XORk{
+public static int xor(int[] a,int k){
+int n=a.length;
+int xr=0;
+Map<Integer,Integer> mpp=new HashMap<>();
+mpp.put(xr,1);
+int cnt=0;
+for(int i=0;i<n ;i++){
+xr=xr^a[i];
+int x=xr^k;
+if(mpp.containsKey(x)){
+cnt+=mpp.get(x);
+}
+if(mpp.containsKey(xr)){
+mpp.put(xr,mpp.get(xr)+1);
+}
+else{
+mpp.put(xr,1);
+}
+}
+return cnt;
+}
+public static void main(String []args){
+int[] a={4,2,2,6,4};
+int k=6;
+int ans=xor(a,k);
+System.out.println("The number of subarrays with xor k is : " + ans);
+}
+}
