7aa07ed33ffc1bc574000d61181275c80c63a9b3
Payal Jain <166532121+Payal-jain123@users.noreply.github.com>
2024-07-22 08:36:51 +0530

    brute

---
+++ MergeOverlaping.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class MergeOverlaping {
+
+    public static List<List<Integer>> mergeOverlappingIntervals(int[][] arr) {
+        int n = arr.length; // size of the array
+        //sort the given intervals:
+        Arrays.sort(arr, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return a[0] - b[0];
+            }
+       });
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) { // select an interval:
+            int start = arr[i][0];
+            int end = arr[i][1];
+
+            //Skip all the merged intervals:
+            if (!ans.isEmpty() && end <= ans.get(ans.size() - 1).get(1)){
+                continue;
+            }
+
+            //check the rest of the intervals:
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j][0] <= end) {
+                    end = Math.max(end, arr[j][1]);
+                } else {
+                    break;
+                }
+            }
+            ans.add(Arrays.asList(start, end));
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {{1, 3}, {8, 10}, {2, 6}, {15, 18}};
+        List<List<Integer>> ans = mergeOverlappingIntervals(arr);
+        System.out.print("The merged intervals are: \n");
+        for (List<Integer>it:ans) {
+            System.out.print(it);
+        }
+        System.out.println();
+    }
+
+}
