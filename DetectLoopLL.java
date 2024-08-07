c9f50e8bb902dc8e161720f3bee904d107b4587b
Payal Jain <166532121+Payal-jain123@users.noreply.github.com>
2024-08-07 20:05:00 +0530

    Add files via upload

---
+++ DetectLoopLL.java
@@ -0,0 +1,48 @@
+class Node{
+int data;
+Node next;
+Node(int data1,Node next1){
+this.data=data1;
+this.next=next1;
+}
+Node(int data1){
+this.data=data1;
+this.next=null;
+}
+}
+class DetectLoopLL{
+static boolean detect(Node head){
+Node slow=head;
+Node fast=head;
+while(fast!=null && fast.next!=null){
+ slow=slow.next;
+ fast=fast.next.next;
+if(slow==fast){
+return true;
+}
+}
+return false;
+}
+public static void main(String[] args){
+Node head=new Node(1);
+Node second=new Node(2);
+Node third=new Node(3);
+Node fourth=new Node(4);
+Node fifth=new Node(5);
+Node sixth=new Node(6);
+Node seven=new Node(7);
+
+        head.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+        fifth.next = sixth;
+sixth.next=seven;
+//seven.next=fifth;//loop exist
+seven.next=null;//loop not exist
+System.out.print(detect(head));
+}
+}
+
+
+
