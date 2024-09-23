512d6f256c20aff8a62424d48c84633b3aa3cc72
Payal Jain <166532121+Payal-jain123@users.noreply.github.com>
2024-09-23 20:53:45 +0530

    Add files via upload

--- MergeSort.java
+++ MergeSort.java
@@ -3,9 +3,9 @@ class MergeSort{
 public static void merge(int[] arr,int low,int mid,int high){
 ArrayList<Integer> temp=new ArrayList<>();
 int left=low;
-int right=mid +1;
+int right=mid+1;
 while(left<=mid && right<=high){
-if(arr[left]<=arr[right]){
+if(arr[left] <=arr[right]){
 temp.add(arr[left]);
 left++;
 }
@@ -26,35 +26,33 @@ for(int i=low;i<=high;i++){
 arr[i]=temp.get(i-low);
 }
 }
-public static void mergeSort(int []arr,int low,int high){
-if(low>=high) return;
+public static void mergesort(int[]arr,int low,int high){
+if(low>=high)return;
 int mid=(low+high)/2;
-mergeSort(arr,low,mid);
-mergeSort(arr,mid+1,high);
+mergesort(arr,low,mid);
+mergesort(arr,mid+1,high);
 merge(arr,low,mid,high);
 }
-
 public static void main(String[] args){
-System.out.println("Enter number of elements: ");
+System.out.println("Enter no. of elements : ");
 Scanner sc=new Scanner(System.in);
 int n=sc.nextInt();
 int[] arr=new int[n];
-System.out.println("Enter the elements:");
+System.out.println("Enter the  elements : ");
 for(int i=0;i<n;i++){
-
 arr[i]=sc.nextInt();
 }
-System.out.println("Before Merge Sort:");
+System.out.println("Before Merge Sort : ");
 for(int i=0;i<n;i++){
-System.out.print(arr[i] +" ");
+System.out.print(arr[i] + " ");
 }
 System.out.println();
-mergeSort(arr,0,n-1);
-System.out.println("After Merge Sort:");
+mergesort(arr,0,n-1);
+System.out.println("After Merge Sort : ");
+for(int i=0;i<n;i++){
+System.out.print(arr[i] + " ");
+}
+System.out.println();
+}}
 
-for(int i=0;i<n;i++){
-System.out.print(arr[i]+ " ");
-}
-System.out.println();
-}
-}
+
