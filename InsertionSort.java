558377524619ff29cf3012f3780306fcf83ed458
Payal Jain <166532121+Payal-jain123@users.noreply.github.com>
2025-03-28 16:40:58 +0530

    Add files via upload

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,28 +1,30 @@
 import java.util.*;
-class 	InsertionSort{
+class InsertionSort{
 public static void insertion(int[] arr,int n){
-for(int i=0;i<=n-1;i++){
+for(int i=0;i<n-1;i++){
 int j=i;
-while(j>0 && arr[j-1]>arr[j]){
+while(j>0 && arr[j-1] > arr[j]){
 int temp=arr[j-1];
 arr[j-1]=arr[j];
 arr[j]=temp;
 j--;
 }
 }
-System.out.println("After Insertion Sort:");
+System.out.println("After Insertion sort : ");
 for(int i=0;i<n;i++){
-System.out.print(arr[i] +" ");
+System.out.print(arr[i] + " ");
 }
 System.out.println();
+
 }
-public static void main(String [] args){
-int[] arr={13,24,63,89,15};
+public static void main(String[] args){
+int []arr={9,46,54,63,12,35};
 int n=arr.length;
-System.out.println("Before Insertion sort");
+System.out.println(" Before Insertion sort : " );
 for(int i=0;i<n;i++){
-System.out.print(arr[i] +" ");
-}System.out.println();
+System.out.print(arr[i] + " ");
+}
+System.out.println();
 insertion(arr,n);
 }
 }
